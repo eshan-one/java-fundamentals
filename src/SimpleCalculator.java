ecc2b667e986c07ebb91236bd73953367ba53e0e
eshan-one <eone84424@gmail.com>
2026-02-09 12:10:14 +0530

    feat: Add SimpleCalculator basic operations

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,9 @@
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Diff: " + (a - b));
+        System.out.println("Prod: " + (a * b));
+        System.out.println("Div: " + (a / b));
+    }
+}
